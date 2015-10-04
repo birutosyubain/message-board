22010b8fb0b0d05550c9f0c19f5f9ae4586719f0
birutosyubain <amarion733@yahoo.co.jp>
2015-10-04 15:10:20 +0000

    first commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
-    # 名前は必須入力かつ20文字以内
+    # 名前は必須入力, 20文字以内
     validates :name , length: {  maximum: 20 } , presence: true
-    # 内容は必須入力かつ2文字以上30文字以下
+    # 内容は必須入力, 2文字以上, 30文字以下
     validates :body, length: { minimum: 2, maximun: 30 }, presence: true
+    # 年齢は必須入力, 数字以外入力できない, 0歳以上
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
 end
